83004ca87dddfde5cb5d5d8a6d1d3a9b07833fa0
0Neocortexx <jonathanemmanueldeoliveira2003@gmail.com>
2022-06-30 10:39:16 -0300

    Atualizacoes

--- js/calcular_media.js
+++ js/calcular_media.js
@@ -2,7 +2,7 @@ function calcularMedia2() {
     var valor1 = parseFloat(document.getElementById("valor_1").value);
     var valor2 = parseFloat(document.getElementById("valor_2").value);
     var media = (valor1 + valor2)/2 ;
-    alert("A média foi: " + media)
+    $("#media").append("Resultado: " + media)
 }
 function calcularMedia3() {
     var valor1 = parseFloat(document.getElementById("valor_1").value);
